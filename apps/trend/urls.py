5a1b9c19702c0c49458b1d03379f91fffd5e9abd
zizle <zizle_lin@163.com>
2019-12-24 16:49:55 +0800

    图表的设置

--- apps/trend/urls.py
+++ apps/trend/urls.py
@@ -9,4 +9,7 @@ urlpatterns = [
     path(r'varieties/group/', views.VarietiesTrendGroupView.as_view()),  # 所有品种下的所有数据组
     re_path(r'^group/(?P<gid>\d+)/table/$', views.GroupRetrieveTablesView.as_view()),  # 单个数据组下的表格，新建数据表
     re_path(r'^table/(?P<tid>\d+)/$', views.RetrieveTableView.as_view()),  # 某个数据表的详情图
+    path(r'chart/', views.ChartView.as_view()),  # 品种图表
+    re_path(r'^(?P<vid>\d+)/chart/$', views.VarietyChartView.as_view()),  # 某品种下的所有图表
+    re_path(r'^chart/(?P<cid>\d+)/$', views.ChartRetrieveView.as_view()),  # 单个图表视图
 ]
