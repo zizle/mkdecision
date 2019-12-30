005615306a06732de6d9306855f3bb0561529f59
zizle <zizle_lin@163.com>
2019-12-30 16:53:00 +0800

    短信通、市场分析

---
+++ apps/info_service/serializers.py
@@ -0,0 +1,30 @@
+# _*_ coding:utf-8 _*_
+# __Author__： zizle
+from rest_framework import serializers
+from .models import MessageLink, MarketAnalysis
+
+
+# 短信通序列化器
+class MessageLinkSerializer(serializers.ModelSerializer):
+    creator = serializers.SerializerMethodField()
+
+    class Meta:
+        model = MessageLink
+        fields = '__all__'
+
+    @staticmethod
+    def get_creator(obj):
+        text = ''
+        if obj.creator:
+            if obj.creator.note:
+                text = obj.creator.note
+            else:
+                text = obj.creator.phone[:3] + '****' + obj.creator.phone[7:]
+        return text
+
+
+# 市场分析序列化器
+class MarketAnalysisSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MarketAnalysis
+        fields = '__all__'
--- apps/info_service/urls.py
+++ apps/info_service/urls.py
@@ -5,5 +5,8 @@ from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
-
+    path('sms/', views.SMSLinkView.as_view()),   # 短信通
+    re_path('^sms/(?P<mid>\d+)/$', views.SMSLinkRetrieveView.as_view()),   # 单个短信通
+    path('market-analysis/', views.MarketAnalysisView.as_view()),  # 市场分析
+    re_path('^market-analysis/(?P<mid>\d+)/$', views.MarketAnalysisRetrieveView.as_view()),   # 单个市场分析
 ]
