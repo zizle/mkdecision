3a8e8164508993701195f4017398428fe0192f90
zizle <zizle_lin@163.com>
2020-01-10 13:51:21 +0800

    顾问服务

--- apps/info_service/urls.py
+++ apps/info_service/urls.py
@@ -5,12 +5,15 @@ from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
-    path('sms/', views.SMSLinkView.as_view()),   # 短信通
-    re_path('^sms/(?P<mid>\d+)/$', views.SMSLinkRetrieveView.as_view()),   # 单个短信通
+    path('sms/', views.SMSLinkView.as_view()),  # 短信通
+    re_path(r'^sms/(?P<mid>\d+)/$', views.SMSLinkRetrieveView.as_view()),  # 单个短信通
     path('market-analysis/', views.MarketAnalysisView.as_view()),  # 市场分析
-    re_path('^market-analysis/(?P<mid>\d+)/$', views.MarketAnalysisRetrieveView.as_view()),   # 单个市场分析
+    re_path(r'^market-analysis/(?P<mid>\d+)/$', views.MarketAnalysisRetrieveView.as_view()),  # 单个市场分析
     path('search-report/', views.SearchReportView.as_view()),  # 调研报告
-    re_path('^search-report/(?P<sid>\d+)/$', views.SearchReportRetrieveView.as_view()),  # 单个调研报告
+    re_path(r'^search-report/(?P<sid>\d+)/$', views.SearchReportRetrieveView.as_view()),  # 单个调研报告
     path('topic-search/', views.TopicSearchView.as_view()),  # 专题研究
-    re_path('^topic-search/(?P<sid>\d+)/$', views.TopicSearchRetrieveView.as_view()),  # 单个专题研究
+    re_path(r'^topic-search/(?P<sid>\d+)/$', views.TopicSearchRetrieveView.as_view()),  # 单个专题研究
+    path('person-train/', views.PersonTrainView.as_view()),  # 顾问服务-人才培养
+    path('dept-build/', views.DeptBuildView.as_view()),  # 顾问服务-部门组建
+    path('inst-examine/', views.InstExamineView.as_view()),  # 顾问服务-制度考核
 ]
--- apps/info_service/views.py
+++ apps/info_service/views.py
@@ -7,10 +7,92 @@ from django.conf import settings
 from django.core.paginator import Paginator
 from django.views.generic import View
 from django.http.response import HttpResponse
+from django.core.files.storage import default_storage
 from .models import MessageLink, MarketAnalysis, SearchReport, TopicSearch
 from .serializers import MessageLinkSerializer, MarketAnalysisSerializer, SearchReportSerializer
 from .forms import MarketAnalysisForm, SearchReportForm, TopicSearchForm
 from utils.client import get_client
+
+
+# 制度考核
+class InstExamineView(View):
+    def post(self, request):
+        machine_code = request.GET.get('mc', None)
+        client = get_client(machine_code)
+        request_user = request.user
+        try:
+            if not client or not client.is_manager:
+                raise ValueError('INVALID CLIENT!')
+            if not request_user or not request_user.is_operator:
+                raise ValueError('登录已过期或不能进行这项操作!')
+            file_path = settings.MEDIA_ROOT + 'info/instExamine/产品服务_制度考核.pdf'
+            default_storage.delete(file_path)  # 删除原来的文件
+            # 获取文件保存
+            default_storage.save(file_path, request.FILES.get('file'))
+            message = '上传成功!'
+            status_code = 201
+        except Exception as e:
+            message = str(e)
+            status_code = 400
+        return HttpResponse(
+            content=json.dumps({'message': message, 'data': []}),
+            content_type='application/json charset=utf-8',
+            status=status_code
+        )
+
+
+# 部门组建
+class DeptBuildView(View):
+    def post(self, request):
+        machine_code = request.GET.get('mc', None)
+        client = get_client(machine_code)
+        request_user = request.user
+        try:
+            if not client or not client.is_manager:
+                raise ValueError('INVALID CLIENT!')
+            if not request_user or not request_user.is_operator:
+                raise ValueError('登录已过期或不能进行这项操作!')
+            file_path = settings.MEDIA_ROOT + 'info/deptBuild/产品服务_部门组建.pdf'
+            default_storage.delete(file_path)  # 删除原来的文件
+            # 获取文件保存
+            default_storage.save(file_path, request.FILES.get('file'))
+            message = '上传成功!'
+            status_code = 201
+        except Exception as e:
+            message = str(e)
+            status_code = 400
+        return HttpResponse(
+            content=json.dumps({'message': message, 'data': []}),
+            content_type='application/json charset=utf-8',
+            status=status_code
+        )
+
+
+# 人才培养
+class PersonTrainView(View):
+    def post(self, request):
+        machine_code = request.GET.get('mc', None)
+        client = get_client(machine_code)
+        request_user = request.user
+        try:
+            if not client or not client.is_manager:
+                raise ValueError('INVALID CLIENT!')
+            if not request_user or not request_user.is_operator:
+                raise ValueError('登录已过期或不能进行这项操作!')
+            file_path = settings.MEDIA_ROOT + 'info/personTra/产品服务_人才培养.pdf'
+            default_storage.delete(file_path)  # 删除原来的文件
+            # 获取文件保存
+            default_storage.save(file_path, request.FILES.get('file'))
+            message = '上传成功!'
+            status_code = 201
+        except Exception as e:
+            message = str(e)
+            status_code = 400
+        return HttpResponse(
+            content=json.dumps({'message': message, 'data': []}),
+            content_type='application/json charset=utf-8',
+            status=status_code
+        )
 
 
 # 专题研究
